dba07e94d73a324a68a5913890ee9515f894afb5
Izolabella <iza.yy10@gmail.com>
2021-11-18 13:32:03 -0500

    code cleanup

--- Classes/Foods.cs
+++ Classes/Foods.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace fatsecret.NET.Classes
+﻿namespace fatsecret.NET.Classes
 {
     public class FoodsResult
     {
--- Classes/Serving.cs
+++ Classes/Serving.cs
@@ -1,10 +1,6 @@
 ﻿using fatsecret.NET.Classes.JSONExt;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace fatsecret.NET.Classes
 {
